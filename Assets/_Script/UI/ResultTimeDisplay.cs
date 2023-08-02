20ca7268d6727a8446c96dc632590ac234a2e17a
かがまーるまーるまーるまーる <36390388+Daikonnbatake@users.noreply.github.com>
2023-08-02 21:28:30 +0900

    リザルトのタイムを表示するコンポーネントとプレハブを追加

---
+++ Assets/_Script/UI/ResultTimeDisplay.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace GJ.UI
+{
+    public class ResultTimeDisplay : MonoBehaviour
+    {
+        [Header("ラベルの設定")]
+        [SerializeField] private Text label;
+        [SerializeField] private string labelText;
+
+        [Space]
+        [Header("タイム表示の設定")]
+        [SerializeField] private Text value;
+        [SerializeField] private string formatString;
+
+        private float seconds;
+
+
+        public float Time
+        {
+            get { return this.seconds; }
+            set
+            {
+                this.seconds = value;
+                this.value.text = this.seconds.ToString(formatString);
+            }
+        }
+
+
+        private void Start()
+        {
+            this.label.text = labelText;
+
+            // デバッグ用に初期値として適当な小数を入れてます.
+            this.Time = Mathf.PI;
+        }
+    }
+}
