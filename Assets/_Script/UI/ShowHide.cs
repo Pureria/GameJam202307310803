b59f7ce00806c7ed101f263599f71c0b6348ef5c
かがまーるまーるまーるまーる <36390388+Daikonnbatake@users.noreply.github.com>
2023-08-02 21:45:58 +0900

    表示/非表示を制御するコンポーネントを追加

---
+++ Assets/_Script/UI/ShowHide.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GJ.UI
+{
+    public class ShowHide : MonoBehaviour
+    {
+        private enum State
+        {
+            [InspectorName("表示")] Show,
+            [InspectorName("非表示")] Hide
+        }
+
+
+        [Header("ゲーム開始時の状態")]
+        [SerializeField] private State initialState;
+
+
+		private void Start()
+		{
+            switch (this.initialState)
+            {
+                case State.Show:
+                    this.gameObject.SetActive(true);
+                    break;
+
+                case State.Hide:
+                    this.gameObject.SetActive(false);
+                    break;
+            }
+		}
+
+
+        public void Show()
+        {
+            this.gameObject.SetActive(true);
+        }
+
+
+        public void Hide()
+        {
+            this.gameObject.SetActive(false);
+        }
+	}
+}
